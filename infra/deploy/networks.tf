1061e2a60284c96bd60cd2a04851c60348eae2a3
Tiam Tee <tiamtee666@gmail.com>
2025-01-19 23:02:42 +0000

    created public and private subnets and internet gateway and route table

---
+++ infra/deploy/networks.tf
@@ -0,0 +1,70 @@
+# create vpc
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+# create internet gateway for internet access
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+# 2 public subnets - 1 for redundancy
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-west-2a"
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-west-2b"
+}
+
+resource "aws_route_table" "public_1" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_2" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_1.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public_2.id
+}
+
+resource "aws_route" "public_1_internet_access" {
+  route_table_id         = aws_route_table.public_1.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route" "public_2_internet_access" {
+  route_table_id         = aws_route_table.public_2.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+# create private subnets
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = false
+  availability_zone       = "eu-west-2a"
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
+  map_public_ip_on_launch = false
+  availability_zone       = "eu-west-2b"
+}
